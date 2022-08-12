fb3ae69d044bdc0100d1245869cc7ba44eb94e3a
zoharkiks <zoharkiks@gmail.com>
2022-08-12 12:59:40 +0530

    Fix fonts

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,20 @@ import Document, {
         <Html>
           <Head>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
+          <link
+            rel="preload"
+            href="/fonts/Blooming Elegant Sans.otf"
+            as="font"
+            type="font/otf"
+            crossOrigin="anonymous"
+          />
+            <link
+            rel="preload"
+            href="/fonts/WorldwideHeadline.ttf"
+            as="font"
+            type="font/otf"
+            crossOrigin="anonymous"
+          />
           </Head>
           <body>
             <Main />
